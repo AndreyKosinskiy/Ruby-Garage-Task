61b272c88a5c751b56f370673722468794030e87
Kosinskiy Andrey <kosinskiy.andrey@ukr.net>
2020-05-04 16:05:30 +0300

    Deleted russian words

--- todo/models.py
+++ todo/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    """ Проект """
+    """ PROJECT """
     name = models.CharField(max_length=256)
 
     def __str__(self):
